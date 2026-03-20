7f769ab93e557f0f7335358843c2cbf1d9780603
조흥재 연구원 <heung@ubisam.com>
2026-03-20 10:18:11 +0900

    20260320-사원, 부서 도메인 설정

---
+++ src/main/java/com/ubisam/exam/domain/Department.java
@@ -0,0 +1,28 @@
+package com.ubisam.exam.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "example_department")
+public class Department {
+  
+  @Id
+  @GeneratedValue
+  private Long id;
+  //부서명
+  private String deptName;
+  //부서장
+  private String manager;
+  //1개의 부서에는 여러명의 사원이 들어올 수 있음.
+  // @OneToMany
+  // private List<Employee> employees;
+
+}
---
+++ src/main/java/com/ubisam/exam/domain/Employee.java
@@ -0,0 +1,33 @@
+package com.ubisam.exam.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "example_employee")
+public class Employee {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  //사원이름
+  private String name;
+  //사원 이메일
+  private String email;
+  //사원 연봉
+  private Integer salary;
+  //사원 입사일
+  private String hireDate;
+  //사원 퇴사일
+  private String terminationDate;
+  //사원 부서
+  //여려명의 사원이 1개의 부서에 속할 수 있음.
+  // @ManyToOne
+  // private Department department;
+
+}
